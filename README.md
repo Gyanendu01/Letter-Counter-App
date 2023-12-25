de84435c43cb784fdaf83d35a1a7dcf7063a414e
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2023-12-25 21:34:19 +0530

    Add description and main.py

---
+++ README.md
@@ -0,0 +1,6 @@
+# Description:
+
+This program is responsible to get a message and a specific letter from a
+user and then count the number of occurrences of that letter in the given message. This
+program should count “H” and “h” as an occurence of h. This program will then display a
+message to the user stating the occurrences of the given letter.
---
+++ main.py
@@ -0,0 +1,24 @@
+print("\n\tWELCOME TO THE LETTER COUNT APP")
+
+def usr_input():
+    usr_name=input("\n\tEnter your username: ")
+    print("\n\tHello {}!\n\tI will count the number of times that a specific letter occurs in a message.".format(usr_name))
+    usr_mssg = input("\n\tPlease enter a message: ")
+    letter_count = input("\tWhich letter would you like to count the occurrences of: ")
+    letter_count = letter_count.lower()
+    usr_mssg = usr_mssg.lower()
+    return letter_count,usr_mssg,usr_name
+
+
+def letter_count(usr_mssg,letter_count,name):
+    count = 0
+    usr_mssg = usr_mssg.split()
+    for i in usr_mssg:
+        for j in i:
+            if j == letter_count:
+                count = count+1
+    print("\n\t{}, your message has {} h's in it".format(name,count))
+
+#MAIN PROGRAM
+usr_data = usr_input()
+letter_count(usr_data[1],usr_data[0],usr_data[2])
