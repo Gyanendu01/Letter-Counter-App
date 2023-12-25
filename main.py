6388a1e8eb0c52dc07d9e40689e58eebed8e983e
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2023-12-25 21:41:25 +0530

    Add more features to the application

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 print("\n\tWELCOME TO THE LETTER COUNT APP")
 
 def usr_input():
-    usr_name=input("\n\tEnter your username: ")
-    print("\n\tHello {}!\n\tI will count the number of times that a specific letter occurs in a message.".format(usr_name))
-    usr_mssg = input("\n\tPlease enter a message: ")
-    letter_count = input("\tWhich letter would you like to count the occurrences of: ")
-    letter_count = letter_count.lower()
-    usr_mssg = usr_mssg.lower()
-    return letter_count,usr_mssg,usr_name
-
+    try:
+        usr_name=input("\n\tEnter your username: ")
+        print("\n\tHello {}!\n\tI will count the number of times that a specific letter occurs in a message.".format(usr_name))
+        usr_mssg = input("\n\tPlease enter a message: ")
+        letter_count = input("\tWhich letter would you like to count the occurrences of: ")
+        letter_count = letter_count.lower()
+        usr_mssg = usr_mssg.lower()
+        return letter_count,usr_mssg,usr_name
+    except ValueError:
+        print("\n\tPlease enter correct information!!")
 
 def letter_count(usr_mssg,letter_count,name):
     count = 0
@@ -22,3 +24,12 @@ def letter_count(usr_mssg,letter_count,name):
 #MAIN PROGRAM
 usr_data = usr_input()
 letter_count(usr_data[1],usr_data[0],usr_data[2])
+
+while True:
+    condn = input("\n\tDo you want to continue with the application? (y/n): ")
+    if condn.lower() == "y":
+        usr_data = usr_input()
+        letter_count(usr_data[1],usr_data[0],usr_data[2])
+    else:
+        print("\n\tTHANK YOU")
+        break
